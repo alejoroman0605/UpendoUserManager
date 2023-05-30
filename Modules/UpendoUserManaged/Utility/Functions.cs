dce851a9d962ccaae7d0f51d3476e6a82082db22
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-05-30 09:00:29 -0400

    Update Functions.cs

--- Modules/UpendoUserManaged/Utility/Functions.cs
+++ Modules/UpendoUserManaged/Utility/Functions.cs
@@ -7,23 +7,46 @@ using static Telerik.Web.UI.OrgChartStyles;
 using DotNetNuke.Common;
 using Upendo.Modules.UpendoUserManaged.Data;
 using Upendo.Modules.UpendoUserManaged.ViewModels;
+using DotNetNuke.Entities.Users;
 
 namespace Upendo.Modules.UpendoUserManaged.Utility
 {
     public class Functions
     {
-        public static IEnumerable<Users> GetUsers()
+        public static IEnumerable<Users> GetUsers(string filter, int portalId)
         {
             ModuleDbContext _context = new ModuleDbContext();
-            var users = _context.Users.Where(s=>s.IsDeleted==false).ToList();
-
-            return users;
+            if (filter == "All")
+            {
+                var users = _context.Users.ToList();
+                return users;
+            }
+            if (filter == "Deleted")
+            {
+                var items = new List<Users>();
+                var deletedUsers = UserController.GetDeletedUsers(portalId).ToArray();
+                foreach (UserInfo u in deletedUsers)
+                {
+                    items.Add(MakeUser(u));
+                }
+                return items;
+            }
+            else
+            {
+                var users = new List<Users>();
+                var getUsers = UserController.GetUsers(portalId).ToArray();
+                foreach (UserInfo u in getUsers)
+                {
+                    users.Add(MakeUser(u));
+                }
+                return users;
+            }
         }
 
         public static IEnumerable<Roles> GetUserRoles(int id)
         {
             ModuleDbContext _context = new ModuleDbContext();
-            var roles = _context.UserRoles.Where(s=>s.UserId == id).ToList();
+            var roles = _context.UserRoles.Where(s => s.UserId == id).ToList();
             var rolesInUser = new List<Roles>();
             foreach (var item in roles)
             {
@@ -34,8 +57,8 @@ namespace Upendo.Modules.UpendoUserManaged.Utility
         public static List<RolesViewModel> GetRoles()
         {
             ModuleDbContext _context = new ModuleDbContext();
-            List<RolesViewModel> roles = new List<RolesViewModel>(); 
-            var rolesDnn= _context.Roles.ToList();
+            List<RolesViewModel> roles = new List<RolesViewModel>();
+            var rolesDnn = _context.Roles.ToList();
             foreach (var item in rolesDnn)
             {
                 var rolViewModel = new RolesViewModel
@@ -47,6 +70,19 @@ namespace Upendo.Modules.UpendoUserManaged.Utility
                 roles.Add(rolViewModel);
             }
             return roles;
+        }
+        public static Users MakeUser(UserInfo u)
+        {
+            var user = new Users()
+            {
+                UserId = u.UserID,
+                Username = u.Username,
+                DisplayName = u.DisplayName,
+                Email = u.Email,
+                IsSuperUser = u.IsSuperUser,
+                IsDeleted = u.IsDeleted,
+            };
+            return user;
         }
     }
 }
