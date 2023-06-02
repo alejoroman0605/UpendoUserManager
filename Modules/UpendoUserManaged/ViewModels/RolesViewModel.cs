b4f57ac944da67162921af39d4b8788dc7606ff1
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-06-02 10:19:47 -0400

    Update RolesViewModel.cs

--- Modules/UpendoUserManaged/ViewModels/RolesViewModel.cs
+++ Modules/UpendoUserManaged/ViewModels/RolesViewModel.cs
@@ -10,5 +10,7 @@ namespace Upendo.Modules.UpendoUserManaged.ViewModels
         public int RoleId { get; set; }
         public int? PortalId { get; set; }
         public string RoleName { get; set; }
+        public bool HasRole { get; set; }
+        public int Index { get; set; }
     }
 }
