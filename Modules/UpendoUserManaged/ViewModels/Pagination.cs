8e6dd03573b1b618825ac1403cba7495ffb33159
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-06-07 11:21:37 -0400

    Create Pagination.cs

---
+++ Modules/UpendoUserManaged/ViewModels/Pagination.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Upendo.Modules.UpendoUserManaged.ViewModels
+{
+    public struct DataTableResponse<T>
+    {
+        public int Take { get; set; }
+        public int Skip { get; set; }
+        public int Page { get; set; }
+        public int GoToPage { get; set; }
+        public int PagesTotal { get; set; }
+        public int RecordsTotal { get; set; }
+        public int RecordsFiltered { get; set; }
+        public string Search;
+        public List<T> Data;
+    }
+
+}
